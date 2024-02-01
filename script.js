9677fd505600659440f96da4f7331413c8f3ae6e
cwatchley4 <cwatchley4@gmail.com>
2024-02-01 10:59:52 -0600

    Color Theme Button

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
 "use strict";
 
+/* Dark - Light Mode Toggle Button */
+
+const button = document.querySelector("[data-theme-icon]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+// const buttonIcon = document.querySelector("data-theme-icon");
+
+function calculateSettingAsThemeString({
+  localStorageTheme,
+  systemSettingDark,
+}) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  } else if (systemSettingDark.matches) {
+    return "dark";
+  } else {
+    return "light";
+  }
+}
+
+function updateButton(buttonEl, isDark) {
+  const newIcon = isDark ? "fa-solid fa-sun" : "fa-solid fa-moon";
+  // use an aria-label if you are omitting text on the button
+  // and using a sun/moon icon, for example
+  buttonEl.setAttribute("class", newIcon);
+  const iconColor = isDark
+    ? (buttonEl.style["color"] = "#ff901b")
+    : (buttonEl.style["color"] = "#333");
+}
+
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+let currentThemeSetting = calculateSettingAsThemeString({
+  localStorageTheme,
+  systemSettingDark,
+});
+
+button.addEventListener("click", (event) => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", newTheme);
+  updateButton(button, newTheme === "dark");
+  updateThemeOnHtmlEl({ theme: newTheme });
+
+  currentThemeSetting = newTheme;
+});
+
 /* NAVBAR DROP DOWN MENU */
 
 const toggleBtn = document.querySelector(".toggle-btn");
